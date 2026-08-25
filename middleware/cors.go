523ea3e48d7141471cabbbfc10448eb980a502f8
hilmibotak <hilmiromadoni@gmail.com>
2026-08-25 07:49:21 +0700

    memperbaiki cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,14 +9,15 @@ import (
 
 func CorsMiddleware() fiber.Handler {
 	allowedOrigins := os.Getenv("CORS_ORIGINS")
+
 	if allowedOrigins == "" {
-		allowedOrigins = "https://frontend-mbg.vercel.app,https://frontend-manbig.vercel.app,https://backendmbg-production.up.railway.app,http://localhost:5000,https://localhost:5000,http://localhost:5173,https://localhost:5173"
+		allowedOrigins = "https://manbigfrontend.vercel.app,http://localhost:5173"
 	}
 
 	return cors.New(cors.Config{
-		AllowOrigins:     allowedOrigins, // Frontend tetap + Railway domain untuk Swagger
+		AllowOrigins:     allowedOrigins,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Requested-With",
 		AllowMethods:     "GET,POST,PUT,DELETE,PATCH,OPTIONS",
-		AllowCredentials: true, // Kembali ke true untuk frontend
+		AllowCredentials: true,
 	})
 }
